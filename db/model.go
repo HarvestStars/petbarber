bb801c7842316bda94f213b435f169214e077b86
unknown <574817827@qq.com>
2020-12-06 12:22:41 +0800

    utf8

--- db/model.go
+++ db/model.go
@@ -14,7 +14,7 @@ var DataBase *gorm.DB
 
 // Setup 启动mysql配置
 func Setup(user string, pwd string, host string, db string) {
-	url := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s", user, pwd, host, db)
+	url := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local&timeout=10s", user, pwd, host, db)
 	var err error
 	DataBase, err = gorm.Open("mysql", url)
 	if err != nil {
